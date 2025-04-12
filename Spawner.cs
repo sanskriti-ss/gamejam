ea5ed76f07284d4debd4f58fbe429b7e7e61639f
Maheswari Bajji <43502655+mbajji@users.noreply.github.com>
2025-04-12 14:35:30 -0700

    Add files via upload

--- Spawner.cs
+++ Spawner.cs
@@ -6,7 +6,7 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] obstaclePrefabs;
     public float obstacleSpawnTime = 2f;
-
+    public float obstacleSpeed = 1f; 
     private float timeUntilObstacleSpawn;
 
     private void Update(){
@@ -16,5 +16,18 @@ public class Spawner : MonoBehaviour
     private void SpawnLoop(){ 
         timeUntilObstacleSpawn += Time.deltaTime;
 
+        if(timeUntilObstacleSpawn >= obstacleSpawnTime){
+            Spawn();
+            timeUntilObstacleSpawn = 0f;
+
+        }
     }
+    private void Spawn(){
+        GameObject obstacleToSpawn = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+        GameObject spawnedObstacle = Instantiate(obstacleToSpawn, transform.position, Quaternion.identity);
+        Rigidbody2D obstacleRB = spawnedObstacle.GetComponent<Rigidbody2D>();
+        obstacleRB.velocity = vector2.left * obstacleSpeed;
+    
+    }
+
 }
